7de4635a366f3095917bc1ac7fa9ef2c2aeb1cfd
Lucifer099-bit01 <rs7729808@gmail.com>
2024-09-18 09:23:46 +0530

    modified

--- script.js
+++ script.js
@@ -2,16 +2,24 @@ let api = "https://api.adviceslip.com/advice";
 let quoteBlock = document.querySelector(".quote");
 let randomQ = document.querySelector(".dice");
 let Qid = document.querySelector("#Qid");
+let dice = document.querySelector("#Img-dice");
+
 
 async function Quotes(){
     let response = await fetch(api);
-    console.log(response);
     let data =  await response.json();
-    console.log(data);
     Qid.innerHTML = `#${data.slip.id}`;
     quoteBlock.innerHTML = `"${data.slip.advice}"`;
+    
+
+
 }
 
 randomQ.addEventListener("click" , () => {
     Quotes();
+    dice.classList.add("diceimg");
+    setTimeout(() => {
+        dice.classList.remove("diceimg")
+    }, 600);
 })
+
